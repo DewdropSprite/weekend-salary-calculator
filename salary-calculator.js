2e21d40fcc6a2b47b95c4c2374de47ab46c3ff06
Tiffany House-Jimenez <tiffanyhousejimenez@gmail.com>
2023-12-03 12:56:01 -0600

    created submit button add employee placeholders and employees table

---
+++ salary-calculator.js
@@ -0,0 +1,15 @@
+onReady();
+
+function onReady(){
+    console.log('Javascript is working!');
+}
+
+function addEmployee(event){
+    console.log("we're adding employees information");
+    const submitFirstName = document.getElementById("firstNameInput")
+    const submitLastName = document.getElementById("lastNameInput")
+    const submitIdNumber = document.getElementById("idNumber")
+    const submitJobTitle = document.getElementById("jobTitle")
+    const submitAnnualSalary = document.getElementById("annualSalary")
+event.preventDefault();
+}
