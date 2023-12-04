47bcaa98ad19c3e2f81438c9451fa51ec1f719a9
Tiffany House-Jimenez <tiffanyhousejimenez@gmail.com>
2023-12-03 18:35:04 -0600

    total monthly in the footer adds teh annual salary and then divides by 12

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function onReady() {
     console.log('Javascript is working!');
 }
 
+let monthlyTotal = 0;
+
 function addEmployee(event) {
     console.log("we're adding employees information");
 
@@ -28,8 +30,15 @@ function addEmployee(event) {
     <td><button onclick="deleteEmployee(event)">Delete</button></td>
     </tr>`;
 
-    document.getElementById("inputData").reset();
+    monthlyTotal += Number(annualSalaryInput);
+    let monthlySalary = monthlyTotal / 12;
+    
+    let mathStuff = document.getElementById("monthlyCalculation");
+    mathStuff.innerHTML = monthlySalary;
 
+
+
+    document.getElementById("inputData").reset();
     event.preventDefault();
 }
 
@@ -39,7 +48,7 @@ function deleteEmployee(event) {
 }
 
 
-/*Clears out the form inputs after a new employee is submitted (10 ms)
+/*
 ✕ Updates the total monthly salary value when a single employee is added (9 ms)
 ✕ Updates the total monthly salary value when multiple employees are added (10 ms)
 ✕ Applies the 'over-budget' CSS class to the footer when the total monthly salary exceeds $20,000 (13 ms)
