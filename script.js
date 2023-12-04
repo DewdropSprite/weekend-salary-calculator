41a9ca63a8ed01ba33992a77056d88f8df0eb19c
Tiffany House-Jimenez <tiffanyhousejimenez@gmail.com>
2023-12-03 18:01:51 -0600

    form data clears after submit button is pushed

--- script.js
+++ script.js
@@ -8,26 +8,18 @@ function addEmployee(event) {
     console.log("we're adding employees information");
 
     let firstNameInput = document.getElementById("firstNameInput").value;
-    console.log("firstName", firstNameInput)
+    //console.log("firstName", firstNameInput)
     let lastNameInput = document.getElementById("lastNameInput").value;
-    console.log("lastName", lastNameInput)
+    //console.log("lastName", lastNameInput)
     let idInput = document.getElementById("idInput").value;
-    console.log("iDNumber", idInput)
+    //console.log("iDNumber", idInput)
     let titleInput = document.getElementById("titleInput").value;
-    console.log("jobTitle", titleInput)
+    //console.log("jobTitle", titleInput)
     let annualSalaryInput = document.getElementById("annualSalaryInput").value;
-    console.log("annualSalary", annualSalaryInput)
-
-    //const firstNameValue = firstName.value;
-    //const lastNameValue = lastName.value;
-    //const iDNumberValue = iDNumber.value;
-    //const jobTitleValue = jobTitle.value;
-    //const annualSalaryValue = annualSalary.value;
-
+    //console.log("annualSalary", annualSalaryInput)
 
     tablePath = document.getElementById("employeeDataTable");
-
-    tablePath.innerHTML +=`<tr>
+    tablePath.innerHTML += `<tr>
     <td>${firstNameInput}</td>
     <td>${lastNameInput}</td>
     <td>${idInput}</td>
@@ -36,6 +28,8 @@ function addEmployee(event) {
     <td><button onclick="deleteEmployee(event)">Delete</button></td>
     </tr>`;
 
+    document.getElementById("inputData").reset();
+
     event.preventDefault();
 }
 
@@ -43,3 +37,10 @@ function deleteEmployee(event) {
     event.target.parentNode.parentNode.remove();
     event.preventDefault();
 }
+
+
+/*Clears out the form inputs after a new employee is submitted (10 ms)
+✕ Updates the total monthly salary value when a single employee is added (9 ms)
+✕ Updates the total monthly salary value when multiple employees are added (10 ms)
+✕ Applies the 'over-budget' CSS class to the footer when the total monthly salary exceeds $20,000 (13 ms)
+*/
