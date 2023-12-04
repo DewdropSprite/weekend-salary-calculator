719be05f06c0250831e0057a50e863ea970c55aa
Tiffany House-Jimenez <tiffanyhousejimenez@gmail.com>
2023-12-03 20:00:23 -0600

    completed assignment

--- script.js
+++ script.js
@@ -4,23 +4,28 @@ function onReady() {
     console.log('Javascript is working!');
 }
 
-let monthlyTotal = 0;
-
+let totalMonthly = 0;
+/** The addEmployee(event) function takes user input in the 
+ * First Name, Last Name, User ID, Title, and Annual Salary fields and populates it
+ * into the employee table when submit button is pushed.
+ */
 function addEmployee(event) {
     console.log("we're adding employees information");
-
+    //assign variables that uses table elements connected to the input fields in html
     let firstNameInput = document.getElementById("firstNameInput").value;
-    //console.log("firstName", firstNameInput)
+    //console.log("First Name:", firstNameInput)
     let lastNameInput = document.getElementById("lastNameInput").value;
-    //console.log("lastName", lastNameInput)
+    //console.log("Last Name:", lastNameInput)
     let idInput = document.getElementById("idInput").value;
-    //console.log("iDNumber", idInput)
+    //console.log("id Number:", idInput)
     let titleInput = document.getElementById("titleInput").value;
-    //console.log("jobTitle", titleInput)
+    //console.log("Job Title:", titleInput)
     let annualSalaryInput = document.getElementById("annualSalaryInput").value;
-    //console.log("annualSalary", annualSalaryInput)
+    //console.log("Annual Salary:", annualSalaryInput)
 
+    //This is filling in the table with variables that were defined above
     tablePath = document.getElementById("employeeDataTable");
+
     tablePath.innerHTML += `<tr>
     <td>${firstNameInput}</td>
     <td>${lastNameInput}</td>
@@ -30,26 +35,33 @@ function addEmployee(event) {
     <td><button onclick="deleteEmployee(event)">Delete</button></td>
     </tr>`;
 
-    monthlyTotal += Number(annualSalaryInput);
-    let monthlySalary = monthlyTotal / 12;
-    
+    //This code is taking the annual salary in the table and getting a monthly total
+    totalMonthly += Number(annualSalaryInput);
+    let monthlySalary = totalMonthly / 12;
+
     let mathStuff = document.getElementById("monthlyCalculation");
     mathStuff.innerHTML = monthlySalary;
 
-
-
+    //This code is identifying if the monthly budget is greater than $20,000 and creating
+    //a message for the user that they are over budget. Colors change using css styling.
+    let monthlySalaryPath = document.getElementById('over-budget');
+    if (monthlySalary > 20000) {
+        document.querySelector('footer').classList.add('over-budget');
+        monthlySalaryPath.innerHTML = "oh no we're over budget!"
+    }
+    //This code clears the user input information after each employee is added
     document.getElementById("inputData").reset();
+    //This code keeps the browser from refreshing when submit button is clicked
     event.preventDefault();
 }
 
+//This function deletes an employee when the delete button is clicked
 function deleteEmployee(event) {
     event.target.parentNode.parentNode.remove();
     event.preventDefault();
 }
 
 
-/*
-✕ Updates the total monthly salary value when a single employee is added (9 ms)
-✕ Updates the total monthly salary value when multiple employees are added (10 ms)
-✕ Applies the 'over-budget' CSS class to the footer when the total monthly salary exceeds $20,000 (13 ms)
-*/
+
+
+
